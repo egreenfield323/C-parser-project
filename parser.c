7c96e47441ddcc83bddf3b2d896624f09a75bae7
Evan Greenfield <evangreenfield@live.com>
2024-01-26 15:34:37 -0500

    fixed file reading and implemented simple syntax checking

--- parser.c
+++ parser.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-#define STR_LEN 100000
+#define STR_LEN 1000 // Adjust as needed
+#define MAX_LINES 1000
+
+char checkSyntax(char stack[][STR_LEN], int lineCount);
 
 int main()
 {
     char fileName[STR_LEN];
     char fileContent[STR_LEN];
-    char *stack[STR_LEN];
+    char stack[MAX_LINES][STR_LEN] = {{0}};
+    int lineCount = 0;
     FILE *file;
 
     printf("Enter a file name\n");
@@ -17,30 +21,82 @@ int main()
     if ((file = fopen(fileName, "r")) == NULL)
     {
         printf("Invalid input: \"%s\"\n", fileName);
+        return 1;
     }
     else
     {
         while (fgets(fileContent, sizeof(fileContent), file) != NULL)
         {
-            for (size_t i = 0; i < strlen(fileContent); i++)
-            {
-                stack[i] += fileContent[i];
-            }
-        }
 
-        printf("%s\n", stack[0]);
-        int parenthesis = 0;
-        for (int i = 0; i < STR_LEN; i++)
-        {
-            if (stack[i] == "(")
-            {
-                parenthesis++;
-            }
+            strncpy(stack[lineCount], fileContent, strlen(fileContent));
+            lineCount++;
         }
-        printf("%d", parenthesis);
 
         fclose(file);
+
+        if (lineCount <= 0)
+        {
+            return 0;
+        }
+        else
+        {
+            checkSyntax(stack, lineCount);
+        }
     }
 
+    return 0;
+}
+
+char checkSyntax(char stack[][STR_LEN], int lineCount)
+{
+    int lineNumber = 1;
+    char parenthesesStack[STR_LEN];
+    int stackIndex = 0;
+
+    // Loop through each line
+    for (int i = 0; i < lineCount; i++)
+    {
+        // Loop through each char in the line
+        for (int j = 0; j < strlen(stack[i]); j++)
+        {
+            // Check for comment
+            if (stack[i][j] == '/' && stack[i][j + 1] == '/')
+            {
+                break;
+            }
+            // Check for opening (,{,[
+            else if (stack[i][j] == '(' || stack[i][j] == '{' || stack[i][j] == '[')
+            {
+                parenthesesStack[stackIndex++] = stack[i][j]; // Push to stack
+            }
+            // Check for closing (,{,[
+            else if (stack[i][j] == ')' || stack[i][j] == '}' || stack[i][j] == ']')
+            {
+                // Check for matching (,{,[
+                if (stackIndex == 0 ||
+                    (stack[i][j] == ')' && parenthesesStack[stackIndex - 1] != '(') ||
+                    (stack[i][j] == '}' && parenthesesStack[stackIndex - 1] != '{') ||
+                    (stack[i][j] == ']' && parenthesesStack[stackIndex - 1] != '['))
+                {
+                    printf("Syntax error at line %d\n", lineNumber);
+                    return 1; // Error
+                }
+                else
+                {
+                    stackIndex--; // Pop from stack
+                }
+            }
+        }
+        lineNumber++; // Move to next line
+    }
+
+    // Check for remaining parentheses
+    if (stackIndex != 0)
+    {
+        printf("Syntax error: Unmatched parentheses\n");
+        return 1; // Error
+    }
+
+    // No syntax errors found
     return 0;
 }
